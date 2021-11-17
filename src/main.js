599a8eb5558344a53b02541a278b654018dca026
nitryuu <kscdh6@gmail.com>
2021-11-17 15:10:23 +0700

    'next'

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import routes from './router'
 import VueSplide from '@splidejs/vue-splide'
 import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, far, fas)
+
 const router = createRouter({
   history: createWebHistory(),
   routes,
@@ -14,4 +22,5 @@ const router = createRouter({
 const app = createApp(App)
 app.use(router)
 app.use(VueSplide)
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.mount('#app')
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -8,6 +8,17 @@ const options =
     perMove: 1,
     gap: '1rem',
     pagination: false,
+    breakpoints: {
+        1023: {
+            perPage: 3
+        },
+        767: {
+            perPage: 2
+        },
+        374: {
+            perPage: 1
+        }
+    }
 }
 
 const categories = [
@@ -89,6 +100,8 @@ const trending = [
     },
 ]
 
+const fullRate = 5
+
 const splide = ref('')
 
 const prevCategory = () => {
@@ -150,6 +163,9 @@ const nextCategory = () => {
                     <div class="banner__title">
                         <span>Good Food Us Good Mood</span>
                     </div>
+                    <div class="banner__image-mobile">
+                        <img src="banner-mobile.png" alt="Banner mobile" />
+                    </div>
                     <div class="banner__description">
                         <span>I would think that conserving our natural resources should be a conservative position: Not to waste food, and not to throw away a lot of the food that we buy.</span>
                     </div>
@@ -206,7 +222,7 @@ const nextCategory = () => {
                                         <span>{{ category.name }}</span>
                                     </div>
                                     <div class="category__total-items">
-                                        <span>{{ category.total }}items</span>
+                                        <span>{{ category.total }} items</span>
                                     </div>
                                 </div>
                             </div>
@@ -225,7 +241,131 @@ const nextCategory = () => {
                     </div>
                 </div>
                 <div class="trending__receipt">
-                    <div class="trending__list"></div>
+                    <div
+                        class="trending__list"
+                        :class="item.category.toLowerCase()"
+                        v-for="(item, index) in trending"
+                        :key="index"
+                    >
+                        <div class="trending__background">
+                            <img src="background.png" alt="Background" />
+                        </div>
+                        <div class="trending__image">
+                            <img :src="item.image" :alt="item.name" />
+                        </div>
+                        <div class="trending__details">
+                            <div class="trending__name">
+                                <span>{{ item.name }}</span>
+                            </div>
+                            <div class="trending__category-name">
+                                <span>{{ item.category }}</span>
+                            </div>
+                            <div class="trending__rating">
+                                <span v-for="rate in item.rating" :key="rate">
+                                    <img src="stars-checked.png" alt="stars checked" />
+                                </span>
+                                <span v-for="rate in (fullRate - item.rating)" :key="rate">
+                                    <img src="stars.png" alt="stars" />
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="trending__button">
+                        <button class="btn trending__all-receipt">ALL Receipt</button>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__wrapper">
+                <div class="footer">
+                    <div class="footer__company">
+                        <div class="footer__logo">
+                            <img src="logo.png" alt="Elemes.id" />
+                        </div>
+                        <div class="footer__address">
+                            <span>Jl. Prof. DR. Satrio No.7, RT.3/RW.3, Karet Kuningan, Kecamatan Setiabudi, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12950</span>
+                        </div>
+                        <div class="footer__social-media">
+                            <div class="footer__social-media-list">
+                                <FontAwesomeIcon :icon="['far', 'envelope']"></FontAwesomeIcon>
+                            </div>
+                            <div class="footer__social-media-list">
+                                <FontAwesomeIcon :icon="['fas', 'phone-alt']"></FontAwesomeIcon>
+                            </div>
+                            <div class="footer__social-media-list">
+                                <FontAwesomeIcon :icon="['fab', 'instagram']"></FontAwesomeIcon>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer__category">
+                        <div class="footer__title">
+                            <span>Categories</span>
+                        </div>
+                        <div class="footer__content">
+                            <div
+                                class="footer__menu-list"
+                                v-for="(category, index) in categories"
+                                :key="index"
+                            >
+                                <div class="footer__category-name">
+                                    <router-link to="/">{{ category.name }}</router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer__about-us">
+                        <div class="footer__title">
+                            <span>About Us</span>
+                        </div>
+                        <div class="footer__content">
+                            <div class="footer__menu-list">
+                                <router-link to="/">About Us</router-link>
+                            </div>
+                            <div class="footer__menu-list">
+                                <router-link to="/">FAQ</router-link>
+                            </div>
+                            <div class="footer__menu-list">
+                                <router-link to="/">Report Problem</router-link>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer__newsletter">
+                        <div class="footer__title">
+                            <span>Newsletter</span>
+                        </div>
+                        <div class="footer__content">
+                            <div class="footer__newsletter-description">
+                                <span>Get now free 50% discount for all products on your first order</span>
+                            </div>
+                            <div class="footer__newsletter-form">
+                                <div class="input-group">
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="Your email address"
+                                    />
+                                    <div class="input-group-append">
+                                        <button
+                                            class="btn footer__newsletter-submit-button"
+                                            type="button"
+                                        >SEND</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="footer__newsletter-email">
+                                <FontAwesomeIcon :icon="['far', 'envelope']"></FontAwesomeIcon>
+                                <span>elemesid@gmail.com</span>
+                            </div>
+                            <div class="footer__newsletter-phone-number">
+                                <FontAwesomeIcon :icon="['fas', 'phone-alt']"></FontAwesomeIcon>
+                                <span>0888 111 2222</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright__wrapper">
+                <div class="copyright">
+                    <span>© 2021 ELEMES ID. ALL RIGHTS RESERVED</span>
                 </div>
             </div>
         </div>
@@ -280,6 +420,10 @@ const nextCategory = () => {
     align-items: center;
 }
 
+.navbar__login {
+    font-weight: 500;
+}
+
 .navbar__sign-up {
     padding: 10px 20px;
     background: #8bac3e;
@@ -328,6 +472,16 @@ const nextCategory = () => {
     margin-bottom: 32px;
 }
 
+.banner__image-mobile {
+    display: none;
+}
+
+.banner__image-mobile img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
 .banner__description {
     font-size: 18px;
     line-height: 29px;
@@ -344,9 +498,11 @@ const nextCategory = () => {
     background: #8bac3e;
     padding: 10px 20px;
     border-radius: 25px;
+    box-shadow: 0 0 13px 3px lightgrey;
 }
 .banner__sign-up a {
     color: white;
+    text-decoration: none;
     font-weight: 500;
 }
 
@@ -358,6 +514,7 @@ const nextCategory = () => {
 
 .banner__about-us a {
     color: black;
+    text-decoration: none;
     font-weight: 500;
 }
 
@@ -370,11 +527,17 @@ const nextCategory = () => {
     height: 100%;
     object-fit: cover;
 }
+
+.category__wrapper {
+    padding: 2rem;
+    margin-top: 2rem;
+}
 .category__header,
 .trending__header {
     font-size: 38px;
     font-weight: 500;
     color: black;
+    line-height: 50px;
 }
 
 .category__title-receipt,
@@ -423,7 +586,8 @@ const nextCategory = () => {
     border-radius: 8px;
 }
 
-.category__background {
+.category__background,
+.trending__background {
     position: absolute;
     top: 0;
     left: 0;
@@ -434,7 +598,8 @@ const nextCategory = () => {
     transition: 0.5s;
 }
 
-.category__background img {
+.category__background img,
+.trending__background img {
     width: 100%;
     height: 100%;
     object-fit: cover;
@@ -460,11 +625,13 @@ const nextCategory = () => {
     background: #f3f7d9;
 }
 
-.category__receipt-list:hover {
+.category__receipt-list:hover,
+.trending__list:hover {
     transform: scale(1.1);
 }
 
-.category__receipt-list:hover .category__background {
+.category__receipt-list:hover .category__background,
+.trending__list:hover .trending__background {
     opacity: 0.09;
 }
 
@@ -494,5 +661,294 @@ const nextCategory = () => {
 
 .category__total-items {
     font-size: 14px;
+}
+
+.trending__wrapper {
+    padding: 1rem;
+}
+
+.trending__receipt {
+    padding: 3rem 0;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 1.1rem;
+}
+
+.trending__list {
+    position: relative;
+    border-radius: 17px;
+    padding: 2rem 0.8rem;
+    transition: 0.5s;
+}
+
+.trending__image {
+    position: relative;
+    margin-bottom: 1.5rem;
+    z-index: 9;
+}
+
+.trending__details {
+    position: relative;
+    z-index: 9;
+}
+
+.trending__name {
+    font-size: 26px;
+    font-weight: 500;
+    color: black;
+}
+
+.trending__category-name {
+    font-size: 18px;
+    font-weight: 500;
+    color: #8bac3e;
+}
+
+.trending__button {
+    grid-column-start: 1;
+    grid-column-end: 5;
+    text-align: center;
+    padding: 2rem;
+}
+
+.trending__button button {
+    background: #8bac3e;
+    border-radius: 100px;
+    color: white;
+    padding: 0.6rem 1.5rem;
+    font-size: 16px;
+    font-weight: 500;
+}
+
+.footer__wrapper {
+    margin-top: 7rem;
+    padding: 2.5rem 1rem;
+    background: #f9fff6;
+    border-radius: 20px;
+}
+
+.footer {
+    display: grid;
+    grid-template-columns: 2fr repeat(2, 1fr) 1.5fr;
+    gap: 1rem;
+}
+
+.footer a {
+    color: #757575;
+    text-decoration: none;
+}
+
+.footer__logo {
+    margin-bottom: 1.5rem;
+}
+
+.footer__address {
+    line-height: 20px;
+    margin-bottom: 1.5rem;
+}
+
+.footer__social-media {
+    display: flex;
+    gap: 0.5rem;
+}
+
+.footer__social-media-list {
+    background: white;
+    border-radius: 50%;
+    padding: 0.8rem;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    transition: 0.5s;
+}
+
+.footer__social-media-list svg {
+    width: 20px;
+    height: 20px;
+}
+
+.footer__social-media-list:hover {
+    background: #8bac3e;
+    color: white;
+}
+
+.footer__company,
+.footer__category,
+.footer__about-us,
+.footer__newsletter {
+    padding: 1.5rem;
+}
+
+.footer__title {
+    font-size: 18px;
+    font-weight: 500;
+    color: black;
+    margin-bottom: 2rem;
+}
+
+.footer__content {
+    display: flex;
+    flex-direction: column;
+    gap: 19px;
+}
+
+.footer__newsletter-description {
+    line-height: 24px;
+}
+
+.footer__newsletter-form input {
+    border: 1px solid black;
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+    font-size: 14px;
+    color: black;
+    line-height: 24px;
+}
+
+.footer__newsletter-form input::placeholder {
+    font-size: 14px;
+    color: black;
+}
+
+.footer__newsletter-form .footer__newsletter-submit-button {
+    font-size: 14px;
+    background: #8bac3e;
+    border: none;
+    color: white;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+}
+
+.footer__newsletter-email,
+.footer__newsletter-phone-number {
+    display: inline-flex;
+    align-items: center;
+    gap: 0.5rem;
+}
+
+.footer__newsletter-email svg,
+.footer__newsletter-phone-number svg {
+    color: #8bac3e;
+    height: 16px;
+    width: 16px;
+}
+.footer__newsletter-email span,
+.footer__newsletter-phone-number span {
+    color: black;
+}
+.copyright__wrapper {
+    padding: 4rem 2rem 2rem;
+    text-align: center;
+}
+.copyright {
+    font-size: 12px;
+}
+
+@media only screen and (max-width: 1365px) {
+    .banner__image img {
+        width: 400px;
+    }
+
+    .trending__receipt {
+        grid-template-columns: repeat(3, 1fr);
+    }
+
+    .trending__button {
+        grid-column-start: 1;
+        grid-column-end: 4;
+    }
+}
+
+@media only screen and (max-width: 1023px) {
+    .navbar__menu {
+        display: none;
+    }
+
+    .banner__wrapper {
+        padding: 4rem 1rem 1rem;
+    }
+
+    .banner__image-mobile {
+        display: block;
+    }
+
+    .banner__image {
+        display: none;
+    }
+
+    .category__carousel:deep() .splide__custom-arrows {
+        display: none;
+    }
+
+    .trending__receipt {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    .trending__button {
+        grid-column-start: 1;
+        grid-column-end: 3;
+    }
+    .footer__wrapper {
+        margin-top: 5rem;
+    }
+
+    .footer {
+        display: block;
+        grid-template-columns: unset;
+    }
+}
+
+@media only screen and (max-width: 767px) {
+    .navbar__wrapper {
+        padding: 1rem 0.2rem;
+        background: white;
+    }
+    .navbar {
+        padding: 0.5rem;
+    }
+    .navbar__logo img {
+        width: 130px;
+    }
+    .navbar__login {
+        display: none;
+    }
+    .navbar__sign-up {
+        font-size: 12px;
+        padding: 8px 13px;
+    }
+    .banner__wrapper {
+        padding: 3rem 0.5rem 1rem;
+    }
+    .banner__title {
+        font-size: 48px;
+        margin-bottom: 20px;
+    }
+
+    .category__wrapper,
+    .trending__wrapper {
+        padding: 2rem 0.5rem;
+    }
+
+    .category__header,
+    .trending__header {
+        font-size: 24px;
+        line-height: 30px;
+    }
+
+    .trending__receipt {
+        display: block;
+        grid-template-columns: unset;
+        gap: unset;
+    }
+
+    .trending__list {
+        margin-bottom: 1.1rem;
+        padding: 2rem 1.5rem;
+    }
+
+    .footer__wrapper {
+        margin-top: 0;
+        padding: 2.5rem 0.5rem;
+    }
 }
 </style>
